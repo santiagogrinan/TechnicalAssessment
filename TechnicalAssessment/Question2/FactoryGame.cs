ecf95b96f0941dad595405d96af8d19d2ed327b4
Santiago Griñán Egea <santiagogrinan@outlook.com>
2022-04-24 22:30:43 +0200

    Add FactoryGames

---
+++ TechnicalAssessment/Question2/FactoryGame.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Question2
+{
+    class FactoryGame
+    {
+        //=====================================================================
+        public Game Point_1()
+        {
+            int suitNumber = 4;
+            int cardPertSuit = 13;
+            bool useWildCard = false;
+            bool suitDecide = false;
+            bool allowTies = true;
+
+            return CreateGame(suitNumber, cardPertSuit, useWildCard, suitDecide, allowTies); 
+        }
+
+        //=====================================================================
+        public Game Point_2()
+        {
+            int suitNumber = 4;
+            int cardPertSuit = 13;
+            bool useWildCard = false;
+            bool suitDecide = true;
+            bool allowTies = true;
+
+            return CreateGame(suitNumber, cardPertSuit, useWildCard, suitDecide, allowTies);
+        }
+
+        //=====================================================================
+        public Game Point_3()
+        {
+            //TO-DO MultipleDesk
+            int suitNumber = 4;
+            int cardPertSuit = 13;
+            bool useWildCard = false;
+            bool suitDecide = true;
+            bool allowTies = true;
+
+            return CreateGame(suitNumber, cardPertSuit, useWildCard, suitDecide, allowTies);
+        }
+
+        //=====================================================================
+        public Game Point_4()
+        {
+            int suitNumber = 4;
+            int cardPertSuit = 13;
+            bool useWildCard = false;
+            bool suitDecide = true;
+            bool allowTies = false;
+
+            return CreateGame(suitNumber, cardPertSuit, useWildCard, suitDecide, allowTies);
+        }
+
+        //=====================================================================
+        public Game Point_5()
+        {
+            int suitNumber = 4;
+            int cardPertSuit = 13;
+            bool useWildCard = true;
+            bool suitDecide = true;
+            bool allowTies = false;
+
+            return CreateGame(suitNumber, cardPertSuit, useWildCard, suitDecide, allowTies);
+        }
+
+        //=====================================================================
+        public Game Point_6()
+        {
+            int suitNumber = 4;
+            int cardPertSuit = 20;
+            bool useWildCard = true;
+            bool suitDecide = true;
+            bool allowTies = false;
+
+            return CreateGame(suitNumber, cardPertSuit, useWildCard, suitDecide, allowTies);
+        }
+
+        //=====================================================================
+        static Game CreateGame(int suitNumber, int cardPerSuit, bool useWildCard, bool suitDecide, bool allowTie)
+        {
+            IGeneratorRandomInt randomGenerate = new Generator();
+            IDeck deck = new Deck(randomGenerate, suitNumber, cardPerSuit, useWildCard ? 1 : 0);
+
+            IComparer<Card> singleComparer = new HighCardCompare(suitDecide);
+            IComparer<Card[]> multipleComparer = new HighCardMultipleCompare(singleComparer);
+
+            return new Game(deck, multipleComparer, allowTie);
+
+        }
+    }
+}
