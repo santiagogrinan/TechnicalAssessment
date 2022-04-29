cab2efce5521edc0a20eafbe68d13fa486513fc7
Santiago Griñán Egea <santiagogrinan@outlook.com>
2022-04-29 14:28:27 +0200

    Question1: Working in simplify the code. Tested with all characters

--- TechnicalAssessment/Question1/Program.cs
+++ TechnicalAssessment/Question1/Program.cs
@@ -10,15 +10,26 @@ namespace Question1
     {
         static void Main(string[] args)
         {
-            Test("This is a test string", "Test 1");
-            Test("other test", "Test 2");
-            Test("abc", "Test 3");
-            Test("abcde", "Test 4");
-            Test("1", "Test 5");
-            Test("This is a large text that allow know if working well with big string", "Test 6");
-            Test("Test special caracter \"&\"", "Test 7");
-        } 
+            try
+            {
+                //Test("This is a test string", "Test 1");
+                //Test("other test", "Test 2");
+                //Test("abc", "Test 3");
+                //Test("abcde", "Test 4");
+                //Test("1", "Test 5");
+                //Test("This is a large text that allow know if working well with big string", "Test 6");
+                //Test("Test special caracter \"&\"", "Test 7");
+                TestAllCharecters();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
 
+
+        }
+
+        //=====================================================================
         static void Test(string text, string testName)
         {
             Encoder encoder = new Encoder();
@@ -44,12 +55,23 @@ namespace Question1
 
             Console.WriteLine("//=====================================================================");
             Console.WriteLine();
-            if (text == plain)
-                Console.WriteLine("Test succeeded");
-            else
-                Console.WriteLine("Test failed");
 
-            Console.WriteLine();
+            if (text != plain)
+                throw new Exception("Test : " + testName + " faild with text : " + text);
+
+            Console.WriteLine("Test succeeded");
+        }
+
+        //=====================================================================
+        static void TestAllCharecters()
+        {
+            char[] text = new char[95];
+
+            //Add uppercase
+            for (int i = 0; i < text.Length; i++)
+                text[i] = (char)((int)' ' + i);
+
+            Test(new string(text), "AllCharecters");
         }
     }
 }
