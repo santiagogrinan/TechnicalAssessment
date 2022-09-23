61bf84c5dd037eb6be8f8222fd8160ffc5537e46
santiago <santiagogrinan@outlook.com>
2022-09-23 11:58:09 +0200

    Create Question1 in F#

---
+++ TechnicalAssessment/F_Question1/Encoder.fs
@@ -0,0 +1,92 @@
+﻿module Encoder
+
+let rotatedChar (value : char, rotate : byte) : char = 
+    char(byte(value) + rotate)
+
+let distanceChar (value_1 : char, value_2 : char) : byte =
+    byte(value_2) - byte(value_1)
+
+let transcodeValue (index : byte) : char option =
+    match index with
+    | i when i < byte 26 -> Some(rotatedChar('A', i))
+    | i when i < byte (26 * 2) -> Some(rotatedChar('a', i - byte(26)))
+    | i when i < byte (26 * 2 + 10) -> Some(rotatedChar('0', i - byte(26 * 2)))
+    | i when i = byte 62 -> Some('+')
+    | i when i = byte 63 -> Some('/')
+    | _ -> None
+
+let transcodeIndex (value : char) : byte option = 
+    match value with
+    | i when (i >= 'A' && i <= 'Z') -> Some(distanceChar ('A', i) + byte(0))
+    | i when (i >= 'a' && i <= 'z') -> Some(distanceChar ('a', i) + byte(26))
+    | i when (i >= '0' && i <= '9') -> Some(distanceChar ('0', i) + byte(26 * 2))
+    | i when i = '+' -> Some(byte 62)
+    | i when i = '/' -> Some(byte 63)
+    | _ -> None
+
+let encode (input : string) : string =
+    let mutable result : string = ""
+    let mutable reflex : int = 0
+
+    let addValueFromPivot (pivot : int) = 
+        match transcodeValue(byte(pivot)) with
+            | Some(x) -> result <- result + string(x)
+            | None -> raise (System.Exception("error"))
+        
+
+    for i in 0 .. (input.Length - 1) do
+        reflex <- reflex <<< 8
+        reflex <- reflex + int(byte(input[i]))
+
+        let rotateFactor : int = ((i % 3) + 1) * 2
+        let mask : int = 63 <<< rotateFactor
+
+        let pivot = (reflex &&& mask) >>> rotateFactor
+        addValueFromPivot pivot
+
+        reflex <- reflex &&& (~~~mask)
+
+        if (rotateFactor = 6) then
+            let newMask = mask >>> 6
+            let newPivot = reflex &&& newMask
+            addValueFromPivot newPivot
+            reflex <- reflex &&& (~~~newMask)
+            
+    match (input.Length % 3) with
+    | 1 -> 
+        reflex <- reflex <<< 4
+        addValueFromPivot reflex
+    | 2 -> reflex <- reflex <<< 2; addValueFromPivot reflex
+    | _ -> result <- result
+
+    result
+
+let decode (input : string) = 
+    let mutable result : string = ""
+    let mutable reflex : int = 0
+
+    let getIndex (caracter : char) : int = 
+        match transcodeIndex(caracter) with
+            | Some(x) -> int x
+            | None -> raise (System.Exception("error"))
+
+    let addValueFromInt (pivot : int) : unit =
+        result <- result + string(char(pivot))
+    
+    for i in 0 .. (input.Length - 1) do
+        reflex <- reflex <<< 6
+        reflex <- reflex + getIndex(input[i])
+
+        let rotateFactor = (3 - (i % 4)) * 2
+
+        if (rotateFactor < 6) then
+            let mask = 255 <<< rotateFactor
+            addValueFromInt((reflex &&& mask) >>> rotateFactor)
+            reflex <- reflex &&& (~~~mask)
+
+    result  
+        
+
+
+    
+
---
+++ TechnicalAssessment/F_Question1/Program.fs
@@ -0,0 +1,23 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let test (message : string) : unit =
+    printfn "//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+    let encoded = Encoder.encode message
+    let decoded = Encoder.decode encoded
+    printfn $"{message} --> {encoded}"
+    printfn $"{encoded} --> {decoded}"
+    printfn ""
+
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    try
+        test "This is a test string"
+        0
+    with 
+        | x -> 
+            printfn $"{x.Message}"
+            1
